f40284cd9623d9699eea04cf11d139d4edaff1de
zyplnu <429510037@qq.com>
2018-06-15 14:56:55 +0800

    four commit

--- src/main/java/com/smart/designmodel/Client.java
+++ src/main/java/com/smart/designmodel/Client.java
@@ -1,5 +1,7 @@
 package com.smart.designmodel;
 
+import java.sql.*;
+
 /**
  * 客户端
  */
@@ -11,6 +13,27 @@ public class Client {
 
         CarModel ocar = new OCar();
         ocar.excet();
+    }
+
+    public void testTransaction() throws SQLException {
+        Connection conn = null;
+        try{
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test");
+            conn.setAutoCommit(false);
+            conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+            Statement statement = conn.createStatement();
+
+            int rows = statement.executeUpdate("insert into t_user values(1,'tom')");
+
+            Savepoint svpt = conn.setSavepoint("svpt1");
+
+            rows = statement.executeUpdate("update t_user set topic_nums = topic_nums + 1" + "where user_id = 1");
+
+            conn.commit();
+        } catch (Exception e){
+            conn.rollback();
+        } finally {
+        }
     }
 
 }
